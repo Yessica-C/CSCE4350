239e91d05ba5bbc879f77bee2c08ed864dbf773f
Yessica-C <41839196+Yessica-C@users.noreply.github.com>
2026-08-24 11:22:59 -0500

    polls tutorial app pt2

--- projectdir/InventorySystem/urls.py
+++ projectdir/InventorySystem/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("testapp/", include("testapp.urls")),
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
